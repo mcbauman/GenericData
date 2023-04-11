4a872dcebbfb8f709b6295ad43b4614c045119a3
mcbauman <m.c.baumann@icloud.com>
2023-04-11 17:48:43 +0200

    basicStyling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,4 +15,51 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style>
+  *{
+    --maincolor:rgba(255, 255, 255, 1);
+    --maincontrast:rgba(0, 0, 0, 0.7);
+  }
+  body{
+    margin: 0 auto;
+    max-width: 1200px;
+    background-color: antiquewhite;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  header{
+    background-color: var(--maincontrast);
+    color: var(--maincolor);
+  }
+  header>nav>a{
+    color: var(--maincolor);
+  }
+  main{
+
+  }
+  section{
+    /* display: flex;
+    justify-content: space-between; */
+  }
+
+  .entryWrapper{
+    display: flex;
+    flex-direction: row;
+    background-color: var(--maincolor);
+    color: var(--maincontrast);
+    margin: 10px;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  .fieldWrapper{
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+  }
+  .row{
+    display: flex;
+    flex-direction: row;
+  }
+  span{
+    font-size: 0.5rem;
+  }
 </style>
--- frontend/src/components/ShowKeys.vue
+++ frontend/src/components/ShowKeys.vue
@@ -5,12 +5,24 @@ const response = useKeyResponseStore();
 </script>
 
 <template>
-<!-- INPUT KEYS -->
-    <section v-for="item in response.Keys">
-      <div><span>name:</span>{{ item.name }}</div>
-      <div><span>type:</span>{{ item.type }}</div>
-    </section>
-    <hr>
-    <section>
-    </section>
+  <!-- INPUT KEYS -->
+  <div v-for="item in response.Keys" class="entryWrapper">
+    <div class="fieldWrapper"><span>name:</span>{{ item.name }}</div>
+    <div class="fieldWrapper"><span>type:</span>{{ item.type }}</div>
+    <div v-if="item.type === 'Number'" class="fieldWrapper">
+      <span>range:</span>{{ item.minRange }} - {{ item.maxRange }}
+    </div>
+    <div v-if="item.type === 'Array'" class="fieldWrapper">
+      <span>Options:</span>
+      <div class="row">
+        <div v-for="field in item.arrayOption">{{ field }}</div>
+      </div>
+    </div>
+    <div v-if="item.type === 'Object'" class="fieldWrapper">
+      <span>Kategorie:</span>
+      <div class="row">
+        <div v-for="field in item.objectEntries">{{ field }}</div>
+      </div>
+    </div>
+  </div>
 </template>
