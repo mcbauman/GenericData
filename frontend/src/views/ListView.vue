ec04b94787332f619d83ef120c1133a4681cc713
mcbauman <m.c.baumann@icloud.com>
2023-04-13 14:36:31 +0200

    LogicGenerallyWorking

---
+++ frontend/src/views/ListView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useKeyResponseStore } from "../stores/keyResonse";
+import { useValueStore } from "../stores/ValueStore"
+import ShowValues from "../components/ShowValues.vue"
+const response = useKeyResponseStore();
+const Values=useValueStore()
+
+Values.requestValues()
+</script>
+
+<template>
+  <section class="showKeys">
+    <ShowValues />
+  </section>
+  <hr>
+</template>
