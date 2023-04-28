da58667e22a7a061039fd70a1cd3af91240f5909
mcbauman <m.c.baumann@icloud.com>
2023-04-28 16:06:44 +0200

    RefactoringFE

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,14 @@
 <script setup>
-import { ref } from 'vue';
 import {userStore} from "./stores/userSettings"
-import StartPage from "./StartPage.vue"
+import StartPage from "./TheStartPage.vue"
+import TheLogin from "./components/TheLogin.vue"
 
-const loginData=ref({})
-const createData=ref({role:"user"})
-const logCreate=ref(true)
 const user=userStore()
 </script>
 
 <template>
   <StartPage v-if="user.token"/>
-  <main v-else class="logBody">
-    <section v-if="logCreate" class="logWindow">
-      <form>
-        <input type="text" v-model="loginData.name" placeholder="UserName">
-        <input type="password" v-model="loginData.password" placeholder="***">
-        <div>
-          <button type="submit" @click.prevent="user.login(loginData)" class="callToAction">LogIn</button>
-          <button class="warning" @click="logCreate=false">Create User</button>
-        </div>
-      </form>
-    </section>
-    <section v-else class="logWindow">
-      <form>
-        <input type="text" v-model="createData.name" placeholder="UserName">
-        <input type="password" v-model="createData.password" placeholder="***">
-        <div>
-          <button type="submit" @click.prevent="user.create(createData)" class="callToAction">Create User</button>
-          <button class="warning" @click="logCreate=true">log in</button>
-        </div>
-      </form>
-    </section>
-  </main>
+  <TheLogin v-else/>
 </template>
 
 <style>
